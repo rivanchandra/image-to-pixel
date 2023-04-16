cc4b5d0baabc021984ec61ac702bf6f6f9d456f7
Rivan Chandra <rivan@genesisdigital.asia>
2023-04-16 23:00:01 +0700

    update the features

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,27 @@
-import * as React from 'react';
+import React, { useState, useRef } from 'react';
 import { PhotoIcon } from '@heroicons/react/24/solid';
+import Slider from '@mui/material/Slider';
+import Checkbox from '@mui/material/Checkbox';
 import { Pixelify } from "react-pixelify";
 
 export default function Home() {
+  const [image, setImage] = useState(null);
+  const [height, setHeight] = useState(30);
+  const [width, setWidth] = useState(30);
+  const [pixelSize, setPixelSize] = useState(10);
+  const [centered, setCentered] = useState(true);
+
+  const handleImageUpload = (event) => {
+    const uploadedImage = event.target.files[0];
+    setImage(URL.createObjectURL(uploadedImage));
+  };
+
   return (
-    <div className="min-h-screen bg-cover bg-no-repeat bg-fixed bg-center bg-gradient-to-r from-purple-100 to-pink-100">
+    <div className="min-h-screen bg-cover bg-no-repeat bg-fixed bg-center bg-gradient-to-r from-purple-200 to-pink-200">
       <div className="h-screen flex flex-row justify-center items-center">
         <div className="w-1/2 p-4 backdrop-filter backdrop-blur-lg rounded-md ">
           <form className="p-4 bg-white rounded-md shadow-md">
-            <label htmlFor="name" className="block font-medium text-gray-700">Name</label>
-            <input type="text" name="name" id="name" className="px-2 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-100" />
-            <label htmlFor="email" className="block mt-4 font-medium text-gray-700">Email</label>
-            <input type="email" name="email" id="email" className="px-2 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-100" />
             <div className="col-span-full">
-              <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
-                Cover photo
-              </label>
               <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
                 <div className="text-center">
                   <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
@@ -25,7 +31,7 @@ export default function Home() {
                       className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
                     >
                       <span>Upload a file</span>
-                      <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                      <input id="file-upload" name="file-upload" type="file" className="sr-only" onChange={handleImageUpload}/>
                     </label>
                     <p className="pl-1">or drag and drop</p>
                   </div>
@@ -33,16 +39,54 @@ export default function Home() {
                 </div>
               </div>
             </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">Height</dt>
+              <dd className="mt-1 leading-6 sm:col-span-2 sm:mt-0">
+                <Slider
+                  size="small"
+                  value={height}
+                  onChange={(e, newValue) => setHeight(newValue)}
+                  aria-label="Small"
+                  valueLabelDisplay="auto"
+                />
+              </dd>
+              <dt className="text-sm font-medium leading-6 text-gray-900">Width</dt>
+              <dd className="mt-1 leading-6 sm:col-span-2 sm:mt-0">
+                <Slider
+                  size="small"
+                  value={width}
+                  onChange={(e, newValue) => setWidth(newValue)}
+                  aria-label="Small"
+                  valueLabelDisplay="auto"
+                />
+              </dd>
+              <dt className="text-sm font-medium leading-6 text-gray-900">Pixel</dt>
+              <dd className="mt-1 leading-6 sm:col-span-2 sm:mt-0">
+                <Slider
+                  size="small"
+                  value={pixelSize}
+                  onChange={(e, newValue) => setPixelSize(newValue)}
+                  aria-label="Small"
+                  valueLabelDisplay="auto"
+                />
+              </dd>
+              <dt className="text-sm font-medium leading-6 text-gray-900">Centered</dt>
+              <dd className="mt-1 leading-6 sm:col-span-2 sm:mt-0">
+                <Checkbox defaultChecked value={centered} onChange={(e, newValue) => setCentered(newValue)}/>
+              </dd>
+            </div>
           </form>
         </div>
         <div className="w-1/2 p-4">
+          {image && 
           <Pixelify
-            src={"https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1528693940/mona-lisa_custom-31a0453b88a2ebcb12c652bce5a1e9c35730a132-s300-c85_boz4fn.jpg"}
-            width={250}
-            height={250}
-            centered={true}
-            pixelSize={8}
-          />
+            src={image}
+            width={width*10}
+            height={height*10}
+            centered={centered}
+            pixelSize={pixelSize}
+          />}
+          
         </div>
       </div>
     </div>
